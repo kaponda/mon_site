e6d647f794c77e6bd4c1b8383500a59a15f1cef4
kaponda <stefkap11@gmail.com>
2022-03-13 03:37:09 +0100

    testaccueil

---
+++ testaccueil.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML >       
+    <html lang="fr">
+     <head>
+        <meta charset="utf-8">
+		<link rel="stylesheet" href="newcss.css"> 
+		<title> accueil</title>
+	 </head>
+     <body>
+	    <div id="bloc_page">
+	      <?php include("structures/entete.php"); ?>
+	      <?php include("structures/menu.php"); ?>
+	      <?php include("structures/section1_accueil.php"); ?>
+	      <?php include("structures/section2_accueil.php"); ?>
+	      <?php include("structures/section3_accueil.php"); ?>
+	      <?php include("structures/pied.php"); ?>
+	    </div>
+	 </body>
+	</html>
